d7f4f2fe86405c50263db31cab39ab125f44c7c5
Byron Walker-Mills <bwalkermills@gmail.com>
2022-10-06 16:57:24 +0100

    Use sveltekit instead of astro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import '../styles/global.css';
+    
+    import Cards from '../components/Cards.svelte';
+    import Form from '../components/Form.svelte';
+</script>
+
+<main class="wrapper">
+    <div class="card-wrapper">
+        <Cards />
+    </div>
+
+    <div class="form-wrapper">
+        <Form />
+    </div>
+
+    <div class="hidden">
+        Thank you!
+        We've added your card details
+        Continue
+    </div>
+
+    <div class="attribution hidden">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Byron</a>.
+    </div>
+</main>
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { writable } from 'svelte/store';
+
+export const name = writable('');
+export const number = writable('');
+export const month = writable('');
+export const year = writable('');
+export const cvc = writable('');
